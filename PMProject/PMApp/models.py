df6e881d3fbfd82476af39bb0a80da72450960fd
Faisal othman <Faisalalsneed@gmail.com>
2022-05-25 00:50:35 +0300

    lab

---
+++ PMProject/PMApp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+#import os
+#from pathlib import path
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, help_text="The name of project")
+    creation_time = models.DateTimeField(auto_now_add=True, help_text="Date created")
+    completion_time = models.DateTimeField(null=True, help_text="Completion time")
+
+    def __str__(self):
+        return self.name + "/" + self.creation_time + "/" + self.completion_time
+
+
+class Task(models.Model):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, )
+    title = models.CharField(max_length=50, help_text="Task Title")
+    description = models.TextField(help_text="Task description=")
+    time_estimation = models.IntegerField(help_text="The task estimation time")
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
