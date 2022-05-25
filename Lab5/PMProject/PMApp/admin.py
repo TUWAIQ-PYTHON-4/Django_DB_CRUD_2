10aa0bbdde4b9c77cd2bd9b02e791f1ff4f7520a
Aaboassoud <a.aboassoud@gmail.com>
2022-05-25 13:16:45 +0300

    updated

--- Lab5/PMProject/PMApp/admin.py
+++ Lab5/PMProject/PMApp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Project,Task
 
-admin.site.register(Project)
-admin.site.register(Task)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creation_time', 'completion_time')
+    date_hierarchy = 'creation_time'
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'time_estimation')
+    list_filter = ('time_estimation',)
+    search_fields = ['title']
+
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Task,TaskAdmin)
