a0984831bffe639254c7129efd6e1c656a705f02
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-25 14:17:10 +0300

    update lab

---
+++ PMProject/PMApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Task
+
+
+# Create your views here.
+
+def index(request):
+    task = Task.objects.all()
+    taskList = []
+
+    for t in task:
+        taskList.append({'task': t})
+    context = {'taskList': taskList}
+    return render(request, 'home.html', context)
+
